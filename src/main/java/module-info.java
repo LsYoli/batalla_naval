e8a31dfcbba9ab69c4378d98896f3126e7cbf925
Maria Baracaldo <maria.baracaldo@correounivalle.edu.co>
2025-12-14 23:19:30 -0500

    Corrección en module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.batalla_naval {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.example.batalla_naval;
 
 
     opens com.example.batalla_naval to javafx.fxml;
